74680a3d18f8a95c1a95e64e02272fab9f7e8454
Simon Zambrovski <simon.zambrovski@holisticon.de>
2023-10-06 11:38:48 +0200

    make it work with 7.20 again

---
+++ process-engine/src/main/java/org/camunda/bpm/engine/impl/batch/removaltime/ProcessSetRemovalTimeJobHandler.java
@@ -0,0 +1,7 @@
+package org.camunda.bpm.engine.impl.batch.removaltime;
+
+/**
+ * Class referenced in JavaDoc only, but contained as an import in org.camunda.bpm.engine.history.SetRemovalTimeToHistoricProcessInstancesBuilder
+ */
+public interface ProcessSetRemovalTimeJobHandler {
+}
--- process-engine/src/main/java/org/camunda/bpm/engine/impl/javax/el/BeanELResolver.java
+++
@@ -1,7 +0,0 @@
-package org.camunda.bpm.engine.impl.javax.el;
-
-/**
- * Should be an interface, but is an abstract class referenced from {@link org.camunda.bpm.application.ProcessApplicationInterface}
- */
-public interface BeanELResolver {
-}
